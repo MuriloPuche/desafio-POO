f3236bba71d7ef3432f4d07b926271287655a620
Murilo <murilo.puche@gmail.com>
2022-07-26 15:35:17 -0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import br.com.desafio.dominio.Curso;
+import br.com.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
+     Curso curso1 = new Curso();
+        curso1.setTiutulo("curso java");
+        curso1.setDescricao("curso de POO");
+        curso1.setCargaHoraria(120);
 
+     Curso curso2 = new Curso();
+        curso2.setDescricao("curso JavaScript");
+        curso2.setDescricao("curso basico de JS");
+        curso2.setCargaHoraria(30);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("mentoria sobre POO");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
--- src/br/com/desafio/dominio/Curso.java
+++ src/br/com/desafio/dominio/Curso.java
@@ -1,8 +1,40 @@
 package br.com.desafio.dominio;
 
 public class Curso {
-        String tiutulo;
-        String descricao;
-        int cargaHoraria;
-        int horaria;
+       private String tiutulo;
+       private String descricao;
+        private int cargaHoraria;
+
+        public String getTiutulo() {
+                return tiutulo;
+        }
+
+        public void setTiutulo(String tiutulo) {
+                this.tiutulo = tiutulo;
+        }
+
+        public String getDescricao() {
+                return descricao;
+        }
+
+        public void setDescricao(String descricao) {
+                this.descricao = descricao;
+        }
+
+        public int getCargaHoraria() {
+                return cargaHoraria;
+        }
+
+        public void setCargaHoraria(int cargaHoraria) {
+                this.cargaHoraria = cargaHoraria;
+        }
+
+        @Override
+        public String toString() {
+                return "Curso{" +
+                        "tiutulo='" + tiutulo + '\'' +
+                        ", descricao='" + descricao + '\'' +
+                        ", cargaHoraria=" + cargaHoraria +
+                        '}';
+        }
 }
